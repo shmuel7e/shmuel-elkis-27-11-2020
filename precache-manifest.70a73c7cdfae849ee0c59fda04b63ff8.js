870bfba569339025692c84d67bfdc91ad93e86d0
Shmuel Elkis <shmuel7e@gmail.com>
2020-12-02 00:10:06 +0200

    Updates

---
+++ precache-manifest.70a73c7cdfae849ee0c59fda04b63ff8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b8309ee50d5815d476890e197a2395",
+    "url": "/shmuel-elkis-27-11-2020/index.html"
+  },
+  {
+    "revision": "a797bbb9336dfe80e4fd",
+    "url": "/shmuel-elkis-27-11-2020/static/css/2.89946ceb.chunk.css"
+  },
+  {
+    "revision": "ac64ea5281adb5ac3352",
+    "url": "/shmuel-elkis-27-11-2020/static/css/main.d1b0d591.chunk.css"
+  },
+  {
+    "revision": "a797bbb9336dfe80e4fd",
+    "url": "/shmuel-elkis-27-11-2020/static/js/2.c8ea942a.chunk.js"
+  },
+  {
+    "revision": "c10c8a49d0ebf0bb8a583624a9f44c2b",
+    "url": "/shmuel-elkis-27-11-2020/static/js/2.c8ea942a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac64ea5281adb5ac3352",
+    "url": "/shmuel-elkis-27-11-2020/static/js/main.2f60df8a.chunk.js"
+  },
+  {
+    "revision": "590bde9b0eeaeed8a8d8",
+    "url": "/shmuel-elkis-27-11-2020/static/js/runtime-main.cdc3ee23.js"
+  },
+  {
+    "revision": "ae4b06bd1a47179f223079bedae2befd",
+    "url": "/shmuel-elkis-27-11-2020/static/media/cloudy.ae4b06bd.jpg"
+  },
+  {
+    "revision": "75add791b52c707b5510b50ad3b4e16a",
+    "url": "/shmuel-elkis-27-11-2020/static/media/fog.75add791.jpg"
+  },
+  {
+    "revision": "f98b234c73f5ce4ef60f85dbe93033d8",
+    "url": "/shmuel-elkis-27-11-2020/static/media/hazy.f98b234c.jpg"
+  },
+  {
+    "revision": "c47a77eac7a37f1f012094d46c123d5f",
+    "url": "/shmuel-elkis-27-11-2020/static/media/night.c47a77ea.jpg"
+  },
+  {
+    "revision": "5912ab73bcf624a0c9fad8a22e3b7ad3",
+    "url": "/shmuel-elkis-27-11-2020/static/media/overcast.5912ab73.jpg"
+  },
+  {
+    "revision": "6c7fa6453da5781f82c2852399ddd4d8",
+    "url": "/shmuel-elkis-27-11-2020/static/media/shower.6c7fa645.jpg"
+  },
+  {
+    "revision": "901e444c0a694cabd02664b365bd3f55",
+    "url": "/shmuel-elkis-27-11-2020/static/media/snow.901e444c.jpg"
+  },
+  {
+    "revision": "8df8dd20ac9285d41bfbf0b5a99252f1",
+    "url": "/shmuel-elkis-27-11-2020/static/media/spinner.8df8dd20.png"
+  },
+  {
+    "revision": "f8a9bc635e92dacbcb05baa6d4fcfb5e",
+    "url": "/shmuel-elkis-27-11-2020/static/media/sunny.f8a9bc63.jpg"
+  },
+  {
+    "revision": "e41c1866eea7893bdd534274195b3162",
+    "url": "/shmuel-elkis-27-11-2020/static/media/thunder.e41c1866.jpg"
+  },
+  {
+    "revision": "277cffb818a553e01e3e143e026a49ac",
+    "url": "/shmuel-elkis-27-11-2020/static/media/windy.277cffb8.jpg"
+  }
+]);
